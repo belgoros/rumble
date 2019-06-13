b2775d0cf43ff15c0959528e6c04afe7926595d7
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-06-13 11:40:35 +0200

    Plug #authenticate function into UserController

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,15 +3,11 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
@@ -36,12 +32,13 @@ defmodule RumblWeb.UserController do
     end
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
       conn
-      |> put_flash(:error, "You must be logged in to access that page") |> redirect(to: Routes.page_path(conn, :index))
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
       |> halt()
     end
   end
