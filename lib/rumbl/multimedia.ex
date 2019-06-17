232446d301056a3b0ad6b68d06b5c0057334bb0e
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-06-17 15:58:33 +0200

    Refactor create/update video functions

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -1,22 +1,10 @@
 defmodule Rumbl.Multimedia do
-  @moduledoc """
-  The Multimedia context.
-  """
-
   import Ecto.Query, warn: false
+
   alias Rumbl.Repo
-
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Accounts
 
-  @doc """
-  Returns the list of videos.
-
-  ## Examples
-
-      iex> list_videos()
-      [%Video{}, ...]
-
-  """
   def list_videos do
     Repo.all(Video)
   end
@@ -37,34 +25,18 @@ defmodule Rumbl.Multimedia do
     from(v in query, where: v.user_id == ^user_id)
   end
 
-  @doc """
-  Gets a single video.
-
-  Raises `Ecto.NoResultsError` if the Video does not exist.
-
-  ## Examples
-
-      iex> get_video!(123)
-      %Video{}
-
-      iex> get_video!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_video!(id), do: Repo.get!(Video, id)
 
-  @doc """
-  Creates a video.
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
 
-  ## Examples
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
 
-      iex> create_video(%{field: value})
-      {:ok, %Video{}}
-
-      iex> create_video(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
@@ -72,49 +44,6 @@ defmodule Rumbl.Multimedia do
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a video.
-
-  ## Examples
-
-      iex> update_video(video, %{field: new_value})
-      {:ok, %Video{}}
-
-      iex> update_video(video, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_video(%Video{} = video, attrs) do
-    video
-    |> Video.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Video.
-
-  ## Examples
-
-      iex> delete_video(video)
-      {:ok, %Video{}}
-
-      iex> delete_video(video)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_video(%Video{} = video) do
-    Repo.delete(video)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking video changes.
-
-  ## Examples
-
-      iex> change_video(video)
-      %Ecto.Changeset{source: %Video{}}
-
-  """
   def change_video(%Video{} = video) do
     Video.changeset(video, %{})
   end
